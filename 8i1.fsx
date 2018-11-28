3d93749a5d646093db7da493d5ffa4f14d4dcc75
Bertram P <bertramherlov@gmail.com>
2018-11-28 10:41:08 +0100

    changes

--- 8i1.fsx
+++ 8i1.fsx
@@ -31,7 +31,18 @@ let rec colourAt (x,y) figure =
          Some ((r1+r2)/2, (g1+g2)/2, (b1+b2)/2)
 
 let circle = Circle((125,125), (125), (0,0,255))
-
+/// <param name "filnavn" "figur" "b" and "h">
+/// filnavn: string in form of the name of the given file you want to produce 
+/// figur: Figure
+/// b: integer in form of width  
+/// h: integer in form of height 
+/// </param name>
+/// <summary>
+/// the functions makes a BitMap (bmp) that takes the form of (b x h), afterwards, by 2 for-loops, it runs through the whole BitMap and checks "colourAt" if one of the given figures is presented, and if this is true, it "prints" a colour onto the BitMap
+/// </summary>
+/// <returns>
+/// The function returns a file, given the name of the String, which is a .png file, and you´re able to open your picture
+/// </returns>
 let MakePicture (filnavn:string) (figur:figure) (b:int) (h:int) =
   let bmp = ImgUtil.mk h b
   for x = 0 to (b-1) do
--- 8i2.fsx
+++ 8i2.fsx
@@ -3,18 +3,7 @@ open ImgUtil
 let circle = Circle((50,50), (45), (0,0,255))
 let rrectangle = Rectangle((40,40), (90,110), (255,0,0))
 let mix = Mix (circle, rrectangle)
-/// <param name "filnavn" "figur" "b" and "h">
-/// filnavn: string in form of the name of the given file you want to produce 
-/// figur: Figure
-/// b: integer in form of width  
-/// h: integer in form of height 
-/// </param name>
-/// <summary>
-/// the functions makes a BitMap (bmp) that takes the form of (b x h), afterwards, by 2 for-loops, it runs through the whole BitMap and checks "colourAt" if one of the given figures is presented, and if this is true, it "prints" a colour onto the BitMap
-/// </summary>
-/// <returns>
-/// The function returns a file, given the name of the String, which is a .png file, and you´re able to open your picture
-/// </returns>
+
 let MakePicture (filnavn:string) (figur:figure) (b:int) (h:int) =
   let bmp = ImgUtil.mk b h
   for x = 0 to (b-1) do
